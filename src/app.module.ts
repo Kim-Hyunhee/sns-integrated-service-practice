be60ceb0edaeb50109283ae9b61cc1b776e7cd1a
Kim-Hyunhee <tta_bi@naver.com>
2024-08-21 12:19:44 +0900

    Modify: Prisma 설치 및 모델 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
